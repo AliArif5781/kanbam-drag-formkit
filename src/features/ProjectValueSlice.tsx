14d5c839f6eec9826be334adb9f2ca1dc5ae06f0
AliArif5781 <aliairf92@gmail.com>
2024-12-01 14:35:13 +0500

    Add ProjectValueSlice.tsx

---
+++ src/features/ProjectValueSlice.tsx
@@ -0,0 +1,53 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Project {
+  id: string;
+  title: string;
+  description: string;
+}
+
+interface ProjectValue {
+  currentTitle: string;
+  currentDescription: string;
+  projects: Project[];
+  selectedProject: Project | null;
+}
+const initialState: ProjectValue = {
+  currentTitle: "",
+  currentDescription: "",
+  projects: [],
+  selectedProject: null,
+};
+
+export const ProjectSlice = createSlice({
+  name: "project-input-value",
+  initialState,
+  reducers: {
+    setCurrentTitle: (state, action: PayloadAction<string>) => {
+      state.currentTitle = action.payload;
+    },
+    setCurrentDescription: (state, action: PayloadAction<string>) => {
+      state.currentDescription = action.payload;
+    },
+    // Add a new project to the projects array in Redux state
+    setAddProject: (state, action: PayloadAction<Project>) => {
+      state.projects.push(action.payload);
+    },
+    // Set the whole list of projects (used for initializing from localStorage)
+    setProjects: (state, action: PayloadAction<Project[]>) => {
+      state.projects = action.payload;
+    },
+    selectedProject: (state, action: PayloadAction<Project>) => {
+      state.selectedProject = action.payload;
+    },
+  },
+});
+
+export const {
+  setCurrentTitle,
+  setCurrentDescription,
+  setAddProject,
+  setProjects,
+  selectedProject,
+} = ProjectSlice.actions;
+export default ProjectSlice.reducer;
