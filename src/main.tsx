9f4f26d98c11abcd83137192ba2659279901e50e
AliArif5781 <aliairf92@gmail.com>
2024-12-12 21:28:35 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/style/sass/main.scss";
 import { Provider } from "react-redux";
-import { store } from "./app/store.ts";
+import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
