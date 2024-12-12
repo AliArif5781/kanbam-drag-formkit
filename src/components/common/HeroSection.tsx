879ccb186c73e5ef52836208239841f118ca51b5
AliArif5781 <aliairf92@gmail.com>
2024-12-12 22:36:35 +0500

    Update HeroSection.tsx

--- src/components/common/HeroSection.tsx
+++ src/components/common/HeroSection.tsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import AddTaskModal from "./AddTaskModal";
 import Navbar from "./Navbar";
