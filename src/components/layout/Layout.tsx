60c4c328a0740184f1676e900fa96ca11a8d64eb
AliArif5781 <aliairf92@gmail.com>
2024-12-12 21:28:35 +0500

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,8 @@
-import Navbar from "../common/Navbar";
 import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
     <div>
-      <Navbar />
       <Outlet />
     </div>
   );
